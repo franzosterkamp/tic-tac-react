6296e2009056c76fd26342e33d15ed2ad2d75f58
franzosterkamp <franz.osterkamp@gmx.net>
2019-11-19 21:31:29 +0100

    update calculateWinner Function

--- src/components/Board.js
+++ src/components/Board.js
@@ -37,26 +37,26 @@ export default function Board() {
       <div className="board-row">
         <Square value={squares[0]} onClick={() => handleClick(0)} />
         <Square value={squares[1]} onClick={() => handleClick(1)} />
+        <Square value={squares[2]} onClick={() => handleClick(2)} />
         <Square value={squares[3]} onClick={() => handleClick(3)} />
-        <Square value={squares[4]} onClick={() => handleClick(4)} />
       </div>
       <div className="board-row">
+        <Square value={squares[4]} onClick={() => handleClick(4)} />
         <Square value={squares[5]} onClick={() => handleClick(5)} />
         <Square value={squares[6]} onClick={() => handleClick(6)} />
         <Square value={squares[7]} onClick={() => handleClick(7)} />
-        <Square value={squares[8]} onClick={() => handleClick(8)} />
       </div>
       <div className="board-row">
+        <Square value={squares[8]} onClick={() => handleClick(8)} />
         <Square value={squares[9]} onClick={() => handleClick(9)} />
         <Square value={squares[10]} onClick={() => handleClick(10)} />
         <Square value={squares[11]} onClick={() => handleClick(11)} />
-        <Square value={squares[12]} onClick={() => handleClick(12)} />
       </div>
       <div className="board-row">
+        <Square value={squares[12]} onClick={() => handleClick(12)} />
         <Square value={squares[13]} onClick={() => handleClick(13)} />
         <Square value={squares[14]} onClick={() => handleClick(14)} />
         <Square value={squares[15]} onClick={() => handleClick(15)} />
-        <Square value={squares[16]} onClick={() => handleClick(16)} />
       </div>
     </div>
   );
